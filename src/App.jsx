489028f23f79fd9c09f7ffc76100635632298303
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-20 15:59:16 +0200

    [UPD] USE STATE E STRUTTURA BASE

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import { useState } from 'react';
 
 function App() {
 
+  const [title, setTtitle] = useState("");
   return (
     <>
-
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>
+            <h1>Movies</h1>
+          </div>
+        </div>
+      </div>
     </>
   )
-}
+};
 
-export default App
+export default App;
