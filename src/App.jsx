8bcb8e1e57029d383c4c303fa093f475de714188
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-21 12:11:28 +0200

    [UPD] CHIAMATA AJAX

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,49 @@
-import axios from 'axios';
+import react from 'react';
 import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [title, setTitle] = useState("");
+  const [titolo, setTitolo] = useState('');
+  const [pulsanti, setPulsanti] = useState([]);
 
-  const handleSubmit = (e) => { e.preventDefault(); }
+  function fetch() {
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=309ed232714c22027bdc63085a7e6a19&query=ritorno+al+fut')
+      .then((response) => {
+        setPulsanti(response.pulsanti)
+      }
+      )
+  };
+
+
+  const handleChange = (event) => {
+    setTitolo(event.target.value);
+  };
 
   return (
-    <>
-      <div className='container'>
-        <h1>Film da cercare</h1>
-        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}
-          placeholder='Inserisci il titolo del film' />
-        <button>Invia</button>
-      </div>
-
-
-    </>
-  )
-};
+    <div className='container'>
+      <h1>Inserisci il titolo del film</h1>
+      <input
+        type="text"
+        id="titoloFilm"
+        value={titolo}
+        onChange={handleChange}
+        placeholder='inserisci il titolo del film'
+      />
+      <button onClick={fetch}>
+        Invia
+      </button>
+      <ul>
+        {pulsanti.map((pulsante) => (
+          <li key={pulsante.id}>{pulsante.titolo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default App;
+
+
+
+
+
