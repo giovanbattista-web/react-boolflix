d4b73da077ca04d9c8615d6b099235c86298b253
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-21 15:05:41 +0200

    [UPD] ARRAY DI OGGETTI CREATI

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import react from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
   const [titolo, setTitolo] = useState('');
   const [pulsanti, setPulsanti] = useState([]);
 
-  function fetch() {
+  const fetch = () => {
     axios.get('https://api.themoviedb.org/3/search/movie?api_key=309ed232714c22027bdc63085a7e6a19&query=ritorno+al+fut')
       .then((response) => {
-        setPulsanti(response.pulsanti)
+        setPulsanti(response.data.results);
+        console.log(response.data.results);
       }
       )
   };
+
+  useEffect(() => {
+    fetch();
+  }, [titolo]);
 
 
   const handleChange = (event) => {
@@ -24,7 +29,7 @@ function App() {
       <h1>Inserisci il titolo del film</h1>
       <input
         type="text"
-        id="titoloFilm"
+        id=""
         value={titolo}
         onChange={handleChange}
         placeholder='inserisci il titolo del film'
@@ -34,7 +39,12 @@ function App() {
       </button>
       <ul>
         {pulsanti.map((pulsante) => (
-          <li key={pulsante.id}>{pulsante.titolo}</li>
+          <li key={pulsante.id}>
+            {pulsante.titolo}
+            {pulsante.original_title}
+            {pulsante.orginale_language}
+            {pulsante.vote_average}
+          </li>
         ))}
       </ul>
     </div>
