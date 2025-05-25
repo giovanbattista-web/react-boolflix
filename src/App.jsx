57ecb198a9aa4c510ed6e7e7e6b25c9feacca326
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-25 08:36:22 +0200

    [UPD] COMPONENTI CARD MODIFICATE

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import react from 'react';
-import Header from './components/Header';
-import Main from './components/Main';
 import BoolProvider from './contexts/BoolContext';
+import { useState, useEffect } from 'react';
+import Layout from './layouts/Layout';
+import axios from 'axios';
+
+const apiMovies = "https://api.themoviedb.org/3/search/movie"
+const apiKey = "309ed232714c22027bdc63085a7e6a19"
 
 function App() {
   return (
     <>
+      const [movies, setMovies] = useState([]);
+      console.log(movies);
       <BoolProvider>
-        <Header />
-        <Main />
+        <Card />
+        <Search />
       </BoolProvider>
     </>
   )
--- src/contexts/BoolContext.jsx
+++ src/contexts/BoolContext.jsx
@@ -1,11 +1,4 @@
-import React from 'react';
 import { createContext } from "react";
 const BoolContext = createContext();
-
-const BoolProvider = ({ children }) => {
-    return (
-        <div>BoolContext</div>
-    )
-};
 
 export default BoolContext;
